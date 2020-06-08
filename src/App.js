a22f7259004a4ae0f3fad35e6da30fa9697318aa
dhruv-chk <dhruv.chk9@gmail.com>
2020-06-08 23:31:58 +0530

    #master: React crash course code.

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import './App.css';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import {v4 as uuid} from 'uuid';
+import About from './components/pages/About';
+
+// https://github.com/bradtraversy/react_crash_todo/blob/master/src/App.js
+
+class App extends Component{
+
+  state = {
+    todos: [
+      {
+        id: uuid(),
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: uuid(),
+        title: 'Dinner with girlfriend',
+        completed: false
+      },
+      {
+        id: uuid(),
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map( todo =>{
+        if(todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo;
+    }
+    ) })
+  }
+
+  //Delete Todo
+  delTodo = (id) =>{
+    this.setState({todos: [...this.state.todos.filter(todo =>
+      todo.id !== id)]});
+  }
+
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title,
+      completed: false
+    }
+    this.setState({ todos : [...this.state.todos, newTodo]});
+  }
+
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header/>
+            <Route exact path ="/" render={props => (
+              <React.Fragment>
+                  <AddTodo addTodo={this.addTodo}/>
+                  <Todos todos={this.state.todos} markComplete = {this.markComplete}
+                  delTodo={this.delTodo}/>
+              </React.Fragment>
+            )}/> 
+
+            <Route path ="/about" component={About}/>           
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+  
+
+export default App;
